7dfd2c75c228e00f7c27c642ab1452f949e5855e
Md Yakoob <md100yakub@gmail.com>
2025-07-08 00:31:17 +0530

    connection error check

--- JudgeaBook/server/models/user.js
+++ JudgeaBook/server/models/user.js
@@ -1,7 +1,9 @@
 require('dotenv').config({ path: require('find-config')('.env') })
 const mongoose = require('mongoose');
 const uri = process.env.MongoDB_URI;
-mongoose.connect(`${uri}/Judgeabook`);
+mongoose.connect(`${uri}/Judgeabook`).then(() => {
+    console.log("Connected to MongoDB")}).catch((err) => {
+    console.error("Error connecting to MongoDB:", err);})
 const userSchema = mongoose.Schema({
     username:String,
     email:String,
