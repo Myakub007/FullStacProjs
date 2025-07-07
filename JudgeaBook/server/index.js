3ff68f9f4dedb756269e3b8bc5bf3e71e1be96e3
Md Yakoob <md100yakub@gmail.com>
2025-07-08 00:59:31 +0530

    minor tweaks

--- JudgeaBook/server/index.js
+++ JudgeaBook/server/index.js
@@ -69,7 +69,7 @@ app.get('/login', (req, res) => {
 
 app.post('/login', async (req, res) => {
     let user = await userModel.findOne({ email: req.body.email })
-    if (!user) return res.send("Email or password is incorrect!");
+    if (!user) return res.redirect('/signup');
     else {
         bcrypt.compare(req.body.password, user.password, (err, result) => {
             if (result) {
