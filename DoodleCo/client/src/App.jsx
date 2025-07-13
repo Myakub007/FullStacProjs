d7564087cae69f4a38973de83ac66971961a1a9e
Md Yakoob <md100yakub@gmail.com>
2025-07-14 00:34:09 +0530

    dragging is working

---
+++ DoodleCo/client/src/App.jsx
@@ -0,0 +1,17 @@
+import { useEffect, useRef } from 'react'
+import './App.css'
+import './index.css'
+import Game from './pages/Game'
+
+
+function App() {
+
+  
+  return (
+    <>
+      <Game />
+    </>
+  )
+}
+
+export default App
---
+++ DoodleCo/client/src/components/Canvas.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { useRef, useEffect } from 'react'
+
+
+const Canvas = () => {
+    const canvasRef = useRef(null);
+    const curMousePos = useRef({x:0,y:0}) 
+
+
+    useEffect(() => {
+
+        const canvas = canvasRef.current;
+        canvas.width = window.innerWidth - 4;
+        canvas.height = window.innerHeight / 2;
+
+        const c = canvas.getContext('2d');
+        c.fillStyle = 'blue'
+        c.fillRect(100, 100, 100, 100);
+        c.fill()
+
+        canvas.addEventListener('mousemove',
+            (e) => {
+                const rect = canvasRef.current.getBoundingClientRect();
+                curMousePos.current.x = e.x - rect.left
+                curMousePos.current.t = e.y - rect.top
+            })
+        canvas.addEventListener('mousedown', (e) => {
+            if (e.buttons === 1) {
+                function animate() {
+                    const {x,y} = curMousePos.current
+                    requestAnimationFrame(animate)
+                    c.beginPath()
+                    c.moveTo(x,y)
+                    // c.moveTo(mouseX,mouseY)
+                    c.lineTo(1000, 1000)
+                    c.stroke()
+                }
+                animate();
+            }
+        })
+    }, [])
+
+    return (
+        <>
+            <canvas ref={canvasRef}></canvas>
+        </>
+    )
+}
+
+export default Canvas
+
