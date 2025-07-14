3d38e8c3bc0af5b20f4c20ab5214a73fdc4bbac1
Md Yakoob <md100yakub@gmail.com>
2025-07-14 14:01:42 +0530

    dragging functionality fixed

--- DoodleCo/client/src/components/Canvas.jsx
+++ DoodleCo/client/src/components/Canvas.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
-import { useRef, useEffect } from 'react'
+import { useRef, useEffect,useState } from 'react'
 
 
 const Canvas = () => {
     const canvasRef = useRef(null);
     const curMousePos = useRef({x:0,y:0}) 
+    const isDrag = useRef(false);
+    const animating = useRef(null);
 
-
+// after calling request animation frame we need to cancel the canimation to stop it from drawing.
     useEffect(() => {
 
         const canvas = canvasRef.current;
@@ -21,24 +23,35 @@ const Canvas = () => {
         canvas.addEventListener('mousemove',
             (e) => {
                 const rect = canvasRef.current.getBoundingClientRect();
-                curMousePos.current.x = e.x - rect.left
-                curMousePos.current.y = e.y - rect.top
-            })
-        canvas.addEventListener('mousedown', (e) => {
-            if (e.buttons === 1) {
-                function animate() {
-                    const {x,y} = curMousePos.current
-                    requestAnimationFrame(animate)
-                    c.beginPath()
-                    c.moveTo(x,y)
-                    // c.moveTo(mouseX,mouseY)
-                    c.lineTo(100, 100)
-                    c.stroke()
+                curMousePos.current.x = e.x -rect.left;
+                curMousePos.current.y = e.y - rect.top;
+                if (isDrag.current) {
+                    function animate() {
+                        const {x,y} = curMousePos.current
+
+                        c.beginPath()
+                        c.moveTo(x,y)
+                        // c.moveTo(mouseX,mouseY)
+                        c.lineTo(100, 100)
+                        c.stroke()
+                    }
+                    animating.current = requestAnimationFrame(animate)
+                    
                 }
-                animate();
-            }
+            })
+            canvas.addEventListener('mousedown', () => {
+                isDrag.current = true
+                animating.current = requestAnimationFrame(animate)
+            })
+            canvas.addEventListener('mouseup',()=>{
+                isDrag.current = false
+                cancelAnimationFrame(animating.current)
+            })
+            canvas.addEventListener('mouseleave',()=>{
+                isDrag.current = false
+                cancelAnimationFrame(animating.current)
         })
-    }, [])
+    }, [isDrag])
 
     return (
         <>
