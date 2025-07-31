d74e5e8143e6b45805d6a27b85fccabd0c0a3ca6
Md Yakoob <md100yakub@gmail.com>
2025-07-31 23:20:24 +0530

    socket error

--- DoodleCo/client/src/context/SocketContext.jsx
+++ DoodleCo/client/src/context/SocketContext.jsx
@@ -3,7 +3,12 @@ import { io } from 'socket.io-client';
 
 const SocketContext = createContext();
 
-export const socket = io('https://doodlecry.onrender.com/') // or your server URL
+export const socket = io('https://doodlecry.onrender.com/'
+ , {
+  withCredentials: true,
+  transports: ['websocket', 'polling'] // fallback support
+}
+) // or your server URL
 
 export const SocketProvider = ({ children }) => (
   <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>
