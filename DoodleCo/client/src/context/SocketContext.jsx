8c80a30909aeccc3f8fa183bc640607b72661e0c
Md Yakoob <md100yakub@gmail.com>
2025-07-31 22:57:54 +0530

    changes for deployment

--- DoodleCo/client/src/context/SocketContext.jsx
+++ DoodleCo/client/src/context/SocketContext.jsx
@@ -3,7 +3,7 @@ import { io } from 'socket.io-client';
 
 const SocketContext = createContext();
 
-export const socket = io('http://localhost:3000' || 'https://doodlecry.onrender.com/'); // or your server URL
+export const socket = io('http://localhost:3000') || io('https://doodlecry.onrender.com/') // or your server URL
 
 export const SocketProvider = ({ children }) => (
   <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>
--- DoodleCo/server/app.js
+++ DoodleCo/server/app.js
@@ -5,12 +5,13 @@ const { Server } = require('socket.io');
 const http = require('http');
 const Port = 3000;
 const crypto = require('crypto');
-const fs = require('fs');
+// const fs = require('fs');
+const path = require('path');
 
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: 'http://localhost:5173' || 'https://full-stac-projs.vercel.app/',
+        origin: ['http://localhost:5173', 'https://full-stac-projs.vercel.app/'],
         methods: ['GET', 'POST'],
     }
 })
@@ -24,7 +25,7 @@ const roomTimers = {};
 
 let words = [];
 if (words.length === 0) {
-    words = fs.readFileSync(__dirname + '/words.txt', 'utf-8').split(',').map(word => word.trim());
+    words = path.join(__dirname, 'words.txt').split(',').map(word => word.trim());
 }
 
 const getRandomWords = (roomID) => {
